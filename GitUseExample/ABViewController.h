adf955b6230b4ae3472d094bff9e2fcaf62d93f7
ronsinda <ronsinda@gmail.com>
2014-08-31 20:45:15 -0700

    First mods

--- GitUseExample/ABViewController.h
+++ GitUseExample/ABViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ABViewController : UIViewController
+- (IBAction)ClickTheButton:(id)sender;
+
+
+
 
 @end
--- GitUseExample/ABViewController.m
+++ GitUseExample/ABViewController.m
@@ -26,4 +26,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)ClickTheButton:(id)sender {
+    NSLog(@"This is  Git Tutorial");
+    
+}
 @end
